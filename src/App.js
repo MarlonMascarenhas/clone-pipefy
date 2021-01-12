dd034beead3d37ae0906ae9e78b3e2d67230eb3b
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-01-12 14:04:45 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <>
+      <Header />
+      
+      
+      <GlobalStyle/>
+    </>
   );
 }
 
---
+++ src/components/Board/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+import List from '../List';
+
+function Board() {
+  return (
+    <Container>
+      <List />
+    </Container>
+  );
+}
+
+export default Board;
